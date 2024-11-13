0206f42ccf69e7abe7cfd7246925c80c023ef02b
yahasop <104791759+yahasop@users.noreply.github.com>
2024-11-13 11:28:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,108 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.69.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ssm_parameter" "amzn2_linux" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket = "terraform-backend-grid-ysolis"
 
-resource "aws_key_pair" "tf-key-pair" {
-  key_name   = "tf-key-pair"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "tf-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "tf-key-pair"
-  provisioner "local-exec" {
-    command = "chmod 400 ./tf-key-pair"
+  tags = {
+    Name = "my-s3-bucket"
   }
 }
-
-resource "aws_vpc" "app" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_hostnames = var.enable_dns_hostnames
-
-  tags = local.common_tags
-}
-
-resource "aws_internet_gateway" "app" {
-  vpc_id = aws_vpc.app.id
-
-  tags = local.common_tags
-}
-
-resource "aws_subnet" "public_subnet1" {
-  cidr_block              = var.vpc_public_subnet1_cidr_block
-  vpc_id                  = aws_vpc.app.id
-  map_public_ip_on_launch = var.map_public_ip_on_launch
-
-  tags = local.common_tags
-}
-
-resource "aws_route_table" "app" {
-  vpc_id = aws_vpc.app.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.app.id
-  }
-
-  tags = local.common_tags
-}
-
-resource "aws_route_table_association" "app_public_subnet1" {
-  subnet_id      = aws_subnet.public_subnet1.id
-  route_table_id = aws_route_table.app.id
-}
-
-resource "aws_security_group" "nginx_sg" {
-  name   = "nginx_sg"
-  vpc_id = aws_vpc.app.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = local.common_tags
-}
-
-resource "aws_instance" "nginx1" {
-  ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public_subnet1.id
-  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
-  key_name               = "tf-key-pair"
-  depends_on             = [local_file.tf-key]
-  user_data              = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
 
   tags = local.common_tags
 }
